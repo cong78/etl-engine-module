54d3ec6160b25ff9a4eaa74de65f51bf55641647
eletonia <elentonia@gmail.com>
2021-03-05 17:01:16 -0500

    changing python script to read values from VolumeMount

--- hello-world-module.py
+++ hello-world-module.py
@@ -4,11 +4,33 @@ import os
 
 def main():
     print("\nHello World Module!")
-    #Read config map values from environment variables
-    print("\nData format is " + os.environ['FORMAT'])
-    print("Data asset name is " + os.environ['NAME'])
-    print("Path to data is " + os.environ['DATAPATH'])
-    
+
+    # Read config map values from volume mount
+    f = open("/hw-module/connection.name", "r")
+    connectionName = f.read()
+    f.close()
+    print("\nConnection name is " + connectionName)
+
+    f = open("/hw-module/connection.format", "r")
+    connectionFormat  = f.read()
+    f.close()
+    print("\nConnection format is " + connectionFormat)
+
+    f = open("/hw-module/connection.credentialLocation", "r")
+    connectionCred  = f.read()
+    f.close()
+    print("\nConnection credential location is " + connectionCred)
+
+    f = open("/hw-module/s3.bucket", "r")
+    s3Bucket  = f.read()
+    f.close()
+    print("\nS3 bucket is " + s3Bucket)
+
+    f = open("/hw-module/s3.endpoint", "r")
+    s3Endpoint  = f.read()
+    f.close()
+    print("\nS3 endpoint is " + s3Endpoint)
+
     print ("\nCOPY SUCCEEDED")
 
 
