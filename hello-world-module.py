1a6ac87485153a038c084d5a16e5ecb3ef4e090a
eletonia <elentonia@gmail.com>
2021-03-09 13:39:34 -0500

    simplifying python code

--- hello-world-module.py
+++ hello-world-module.py
@@ -9,21 +9,15 @@ def main():
 
     # Read config map values from volume mount
     with open('/hw-module/conf.yaml', 'r') as stream:
-        content = yaml.load(stream)
+        content = yaml.safe_load(stream)
         for key,val in content.items():
             if "data" in key:
-                x = val[0]
-                for k,v in x.items():
-                    if "connection.name" in k:
-                        connectionName = v
-                    if "connection.format" in k:
-                        connectionFormat = v
-                    if "connection.credentialLocation" in k:
-                        connectionCred = v
-                    if "s3.bucket" in k:
-                        s3Bucket = v
-                    if "s3.endpoint" in k:
-                        s3Endpoint = v
+                data = val[0]
+                connectionName = data["connection.name"]
+                connectionFormat = data["connection.format"]
+                connectionCred = data["connection.credentialLocation"]
+                s3Bucket = data["s3.bucket"]
+                s3Endpoint = data["s3.endpoint"]
 
     print("\nConnection name is " + connectionName)
     print("\nConnection format is " + connectionFormat)
